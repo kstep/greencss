bc8d61dc2cd178a982f07ecc890ad76b35756742
Konstantin Stepanov <me@kstep.me>
2011-04-19 00:44:37 +0300

    :nth-child() series of selectors supported

--- lexer/tokens/statements.py
+++ lexer/tokens/statements.py
@@ -1,5 +1,5 @@
 from lexer.tokens import Selector, Property, ComplexProperty, Variable, Macro, Rule, Arguments
-from lexer.tokens.basic import identifier, spaces
+from lexer.tokens.basic import identifier, spaces, digit
 from lexer.parsers.parsers import _, EOL, W, inf
 from lexer.tokens.expressions import value, values, cmacrocall
 from lexer.parsers.filters import join
@@ -13,17 +13,26 @@ cid = ('#' - identifier) / join
 cclass = ('.' - identifier) / join
 cpclass = (':' - identifier) / join
 
+sign = _(['+-'])
+number = digit*(1,inf)
+nthchild_arg = (
+        _('even') |
+        _('odd')  |
+        sign.opt - number - 'n' - sign - number |
+        sign.opt - number
+        ) / join
+nthchild = (':nth-' - _('last-').opt - (_('child')|_('of-type')) - '(' - nthchild_arg - ')') / join
+
 ctag = identifier | cpholdr
-cmodifier = cid | cclass | cpclass
+cmodifier = cid | cclass | cpclass | nthchild
 catom = (
         (cmodifier * (1, inf)) | (ctag - cmodifier * inf)
         ) / join
 
-cexpression_tail = spaces/0 - ([',+>'] - spaces/0).opt - catom
+cexpression_tail = spaces/0 - (['+>'] - spaces/0).opt - catom
 cexpression = catom - cexpression_tail*inf - spaces/0
 
-
-cselector = cexpression - _(':')/0 - EOL >> Selector
+cselector = cexpression.commalist - _(':')/0 - EOL >> Selector
 cproperty = (
         identifier - (':' - spaces)/0 - values - EOL >> Property |
         identifier - _('>')/0 - EOL -
@@ -33,11 +42,11 @@ cproperty = (
         )
 
 cmacro = (
-        (W('@define').push()/0 - identifier -
+        W('@define').push()/0 - identifier -
             -(identifier.commalist.surround >> Arguments) -
         (_(':'))/0 - EOL -
-        (cproperty.indent)
-        ) >> Macro).drop()
+        cproperty.indent >> Macro
+        ).drop()
 
 _crule = _(lambda inp: crule(inp))
 crule = (
