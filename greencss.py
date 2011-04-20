40b1d618c63a63767ce56b4c89a750337328d480
Konstantin Stepanov <me@kstep.me>
2011-04-20 04:36:43 +0300

    convert() method is a little clearer

--- greencss.py
+++ greencss.py
@@ -5,6 +5,8 @@ from lexer.tokens.statements import vardef, cmacro, crule
 _parser = (vardef/0 | cmacro/0 | crule) * inf
 
 def convert(data):
-    token, rest = _parser(clear_lines(data))
+    data = clear_lines(data)
+    token, rest = _parser(data)
+    #print token, rest
     return ''.join(t.render() for t in token)
 
