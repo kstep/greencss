dba09b31727521bb9d7b8008d8980e4e0b9546d9
Konstantin Stepanov <me@kstep.me>
2011-04-26 17:46:45 +0300

    greenc.py autodetect CGI environment

--- greenc.py
+++ greenc.py
@@ -3,17 +3,23 @@
 
 from __future__ import with_statement
 
-import sys  # , os
+import sys, os
 from greencss import convert
 
 try:
-    filename = sys.argv[1]
+    filename = os.path.abspath(sys.argv[1])
 except IndexError:
     filename = None
 
+
 if filename:
+    if 'SERVER_NAME' in os.environ:  # CGI mode
+        print "Content-Type: text/css\n"
+        print '/*', filename, '*/'
     with open(filename, 'r') as f:
-        print convert(f.read())
+        data = f.read()
+        #print data
+        print convert(data)
 else:
     # TODO: convert standard input
     pass
